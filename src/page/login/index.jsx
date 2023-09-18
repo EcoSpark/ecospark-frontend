5d81d22157509ac77e5a16b5f4b3c634d18e576d
Samuel Ramos de Almeida <samuks2005@outlook.com>
2023-09-17 23:03:15 -0300

    feat: criando componente de login-page

---
+++ src/page/login/index.jsx
@@ -0,0 +1,10 @@
+import * as S from "./styled";
+import { PopUp } from "../../components/pop-up/index";
+
+export const LoginPage = () => {
+  return (
+    <S.WrapperStyled>
+      <PopUp />
+    </S.WrapperStyled>
+  );
+};
