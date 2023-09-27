070eb6f4e7cf9b03bde64da7737837bb90227a24
Samuel Ramos de Almeida <samuks2005@outlook.com>
2023-09-27 01:20:15 -0300

    feat: instalando libs

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import { RouterProvider } from "react-router-dom";
 import { routers } from "./routes/routes";
 import GlobalStyle from "./style/global";
 
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { queryClient } from "./utils/react-query";
+
 function App() {
   return (
     <>
-      <React.StrictMode>
-        <GlobalStyle />
-        <RouterProvider router={routers} />
-      </React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <React.StrictMode>
+          <GlobalStyle />
+          <RouterProvider router={routers} />
+        </React.StrictMode>
+        <ToastContainer />
+      </QueryClientProvider>
     </>
   );
 }
