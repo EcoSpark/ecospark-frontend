863fcdcf595d824988fb4743df611760ea9f765e
Samuka <samuks2005@outlook.com>
2023-11-10 21:20:28 -0300

    feat: tirando styled-components e integrando SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import * as React from "react";
 import { RouterProvider } from "react-router-dom";
-import { routers } from "./routes/routes";
-import GlobalStyle from "./style/global";
-
-import { QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { routers } from "./routes/routes";
 import { queryClient } from "./utils/react-query";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./style/main.sass";
 
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <React.StrictMode>
-          <GlobalStyle />
           <RouterProvider router={routers} />
         </React.StrictMode>
         <ToastContainer />
