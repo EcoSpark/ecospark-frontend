1b144ef55e5b9ab0523bc061720914cccd09ccc4
Laís <lais.bertoncello@aluno.ufabc.edu.br>
2023-11-10 21:21:50 -0300

    feat: criando component-header e melhorando imagens

---
+++ src/components/header/index.jsx
@@ -0,0 +1,10 @@
+import { EcoSparkWhite } from "../../assets/logos/ecospark-white";
+import "./header.style.sass";
+
+export const Header = () => {
+  return (
+    <header>
+      <EcoSparkWhite />
+    </header>
+  );
+};
