f2306e84777d76a80580dbad59985927daac837d
Samuka <samuks2005@outlook.com>
2023-11-12 17:12:43 -0300

    feat: finalizando

--- src/page/products/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Footer = styled.footer`
-  height: 100px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: transparent;
-`;
